0ca314551f7e87fe140cb3b01d6a8bff9dc93196
yerhabe <yeredhaber@gmail.com>
2014-03-14 13:02:16 -0400

    ...

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(insp
 if cmd_subfolder not in sys.path:
 	sys.path.insert(0, cmd_subfolder)
 
-from flask import Flask, request
+from flask import Flask
 from telapi import rest
 
 app = Flask(__name__)
