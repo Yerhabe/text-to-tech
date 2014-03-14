36c7d58a18b8b8e91a2e4abbdd190c3a5f797f97
yerhabe <yeredhaber@gmail.com>
2014-03-14 18:07:01 -0400

    sms set up

--- hello.py
+++ hello.py
@@ -12,10 +12,22 @@ def hello():
 @app.route('/sms')
 def sms():
 	sms_body = request.args.get('Body', None)
-	return sms_body
-
-
 	
+	account_sid = 'ACec889084f5a483fc17fd4115b4be2b5e'
+    auth_token = '86b901cc0c16475694d03c8e8bc9523b'
+    client = rest.Client(account_sid, auth_token)
+    account = client.accounts[client.account_sid]
+ 
+    from_number = request.args.get('To', None)
+    to_number = request.args.get('From', None)
+    body = "body: " + sms_body 
+ 
+    account.sms_messages.create(from_number, to_number, body)
+ 
+	return "SMS Received"
+
+
+
 
 if __name__ == '__main__':
 	app.run()
