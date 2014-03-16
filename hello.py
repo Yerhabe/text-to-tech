67716b821d215145bba0a66bee38fbcb50188c43
yerhabe <yeredhaber@gmail.com>
2014-03-15 22:37:44 -0400

    ...

--- hello.py
+++ hello.py
@@ -13,16 +13,16 @@ def hello():
 def sms():
 	sms_body = request.args.get('Body', None)
 	
-	account_sid = '{DIcf19ee8ca57e4bf9983742845c40d1a6}' 
-    auth_token = '{86b901cc0c16475694d03c8e8bc9523b}'
-    client = rest.Client(account_sid, auth_token)
-    account = client.accounts[client.account_sid]
+	account_sid = 'ACec889084f5a483fc17fd4115b4be2b5e' 
+	auth_token = '86b901cc0c16475694d03c8e8bc9523b'
+	client = rest.Client(account_sid, auth_token)
+	account = client.accounts[client.account_sid]
  
-    from_number = request.args.get('To', None)
-    to_number = request.args.get('From', None)
-    body = "body: " + sms_body 
+	from_number = request.args.get('To', None)
+	to_number = request.args.get('From', None)
+	body = "body: " + sms_body 
  
-    account.sms_messages.create(from_number, to_number, body)
+	account.sms_messages.create(from_number, to_number, body)
  
 	return "SMS Received"
 
