8cf22fa19de1979c0ca7684435d597ac404000b5
yerhabe <yeredhaber@gmail.com>
2014-03-15 23:04:00 -0400

    trying voice response

--- hello.py
+++ hello.py
@@ -24,7 +24,10 @@ def sms():
 	to_number = request.args.get('From', None)
 	body = "body: " + sms_body 
  
-	account.sms_messages.create(to_number = "732-966-2819", from_number = "917-723-8337", body = "success")
+	# account.sms_messages.create(to_number = "732-966-2819", from_number = "917-723-8337", body = "success")
+
+	voice_url   = 'http://db.tt/YtLJgpa8'
+	account.calls.create(from_number="+19177238337", to_number="+17329662819", url=voice_url)
  
 	return "SMS Received"
 
