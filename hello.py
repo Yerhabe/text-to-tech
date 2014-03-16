52c3f98ae4400f8d419c12f4330ab758f4573781
yerhabe <yeredhaber@gmail.com>
2014-03-15 22:32:01 -0400

    ...

--- hello.py
+++ hello.py
@@ -13,8 +13,8 @@ def hello():
 def sms():
 	sms_body = request.args.get('Body', None)
 	
-	account_sid = 'DIcf19ee8ca57e4bf9983742845c40d1a6' 
-    auth_token = '86b901cc0c16475694d03c8e8bc9523b'
+	account_sid = '{DIcf19ee8ca57e4bf9983742845c40d1a6}' 
+    auth_token = '{86b901cc0c16475694d03c8e8bc9523b}'
     client = rest.Client(account_sid, auth_token)
     account = client.accounts[client.account_sid]
  
